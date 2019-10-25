0a72a11a88f7afee2440cd018dbfbe00dcd76ba6
Hammastanko <45659843+Hammastanko@users.noreply.github.com>
2019-10-25 16:52:24 +0300

    working

--- src/index.js
+++ src/index.js
@@ -20,127 +20,176 @@ function initializeCode() {
 
   const q0button = document.getElementById("q0");
   q0button.addEventListener("mousedown", event => {
-    console.log("successful press");
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q0);
     event.stopPropagation();
   });
   const q1button = document.getElementById("q1");
   q1button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q1);
     event.stopPropagation();
   });
   const q2button = document.getElementById("q2");
   q2button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q2);
     event.stopPropagation();
   });
   const q3button = document.getElementById("q3");
   q3button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q3);
     event.stopPropagation();
   });
   const q4button = document.getElementById("q4");
   q4button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q4);
     event.stopPropagation();
   });
   const q5button = document.getElementById("q5");
   q5button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q5);
     event.stopPropagation();
   });
   const q6button = document.getElementById("q6");
   q6button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q6);
     event.stopPropagation();
   });
   const q7button = document.getElementById("q7");
   q7button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q7);
     event.stopPropagation();
   });
   const q8button = document.getElementById("q8");
   q8button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q8);
     event.stopPropagation();
   });
   const q9button = document.getElementById("q9");
   q9button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q9);
     event.stopPropagation();
   });
   const q10button = document.getElementById("q10");
   q10button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q10);
     event.stopPropagation();
   });
   const q11button = document.getElementById("q11");
   q11button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q11);
     event.stopPropagation();
   });
   const q12button = document.getElementById("q12");
   q12button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q12);
     event.stopPropagation();
   });
   const q13button = document.getElementById("q13");
   q13button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q13);
     event.stopPropagation();
   });
   const q14button = document.getElementById("q14");
   q14button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q14);
     event.stopPropagation();
   });
   const q15button = document.getElementById("q15");
   q15button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q15);
     event.stopPropagation();
   });
   const q16button = document.getElementById("q16");
   q16button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q16);
     event.stopPropagation();
   });
   const q17button = document.getElementById("q17");
   q17button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q17);
     event.stopPropagation();
   });
   const q18button = document.getElementById("q18");
   q18button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q18);
     event.stopPropagation();
   });
   const q19button = document.getElementById("q19");
   q19button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q19);
     event.stopPropagation();
   });
   const q20button = document.getElementById("q20");
   q20button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q20);
     event.stopPropagation();
   });
   const q21button = document.getElementById("q21");
   q21button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q21);
     event.stopPropagation();
   });
   const q22button = document.getElementById("q22");
   q22button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q22);
     event.stopPropagation();
   });
   const q23button = document.getElementById("q23");
   q23button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q23);
     event.stopPropagation();
   });
   const q24button = document.getElementById("q24");
   q24button.addEventListener("mousedown", event => {
+    window.clearInterval(window.barInterval);
+    window.clearInterval(window.timerInterval);
     newMove(q24);
     event.stopPropagation();
   });
@@ -184,6 +233,35 @@ function switchTurn() {
 
 function progress() {
   var timeleft = 10;
+  window.barInterval = setInterval(function() {
+    document.getElementById("progressBar").style.width =
+      (10 - timeleft) * 10 + "%";
+    timeleft -= 1;
+    if (timeleft <= 0) {
+      window.clearInterval(window.barInterval);
+    }
+  }, 1000);
+
+  var textTimer = 10;
+  window.timerInterval = setInterval(function() {
+    document.getElementById("time").innerHTML = textTimer;
+    if (textTimer <= 0) {
+      window.clearInterval(window.timerInterval);
+      switchTurn();
+    }
+    textTimer -= 1;
+  }, 1000);
+
+  /*
+  document.getElementById("progressBar").style.width = "70%";
+  console.log("successful press");
+
+  */
+}
+
+/*
+function progress() {
+  var timeleft = 10;
   var downloadTimer = setInterval(function() {
     timeleft -= 1;
     document.getElementById("progressBar").value = 10 - timeleft;
@@ -207,7 +285,7 @@ function clearProgress() {
   document.getElementById("progressBar").value = 0;
   document.getElementById("time").innerHTML = 10;
 }
-
+*/
 function checkWin(move) {
   var result = false;
 
@@ -257,27 +335,5 @@ function changeBackground(turn, square) {
   } else {
     square.style.backgroundColor = "#FA8072";
     //RGB code 250, 128, 114
-  }
-
-  function move() {
-    var elem = document.getElementById("myBar");
-    var width = 0;
-    var id = setInterval(frame, 50);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        document.getElementById("myP").className =
-          "w3-text-green w3-animate-opacity";
-        document.getElementById("myP").innerHTML =
-          "Successfully uploaded 10 photos!";
-      } else {
-        0;
-        width++;
-        elem.style.width = width + "%";
-        var num = (width * 1) / 10;
-        num = num.toFixed(0);
-        document.getElementById("demo").innerHTML = num;
-      }
-    }
   }
 }
